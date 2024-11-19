ca3411d6107f28911d816448044ea20e2d7e4ccf
Dylan Evans <dylbalke03@gmail.com>
2024-11-18 21:18:42 -0500

    17 - SHOWING MUTATION PROGRESS

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -21,6 +21,7 @@ const TodoForm = () => {
         ...(todos || []),
         savedToDo,
       ]);
+      if (ref.current) ref.current.value = "";
     },
   });
   const ref = useRef<HTMLInputElement>(null);
@@ -47,7 +48,9 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>
+            {addTodo.isLoading ? "Saving..." : "Save"}
+          </button>
         </div>
       </form>
     </>
